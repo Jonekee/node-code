cd6dc40a439e20adeda5e9554450941d1dc97f19
John Allen <johnfallen@gmail.com>
2015-04-08 14:38:13 -0400

    MAJOR: added class template file

---
+++ ClassTemplate.js
@@ -0,0 +1,14 @@
+/**
+ * @fileOverview  	I am a description of the code.
+ * @author 			John Allen <johnfallen@gmail.com>
+ * @version 		1.0.0
+ * @module 			ClassTemplate.js
+ */
+
+/* *************************** Required Classes **************************** */
+
+/* *************************** Constructor Code **************************** */
+
+/* *************************** Public Methods ****************************** */
+
+/* *************************** Private Methods ***************************** */
